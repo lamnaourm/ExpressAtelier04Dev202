797242329cdd88c29487b0aeec62721ebe7bd448
lamnaourm <mohammed.lamnaour@gmail.com>
2022-12-26 18:06:42 +0100

    route 3

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { v4: uuid} = require('uuid')
+const path = require('path')
 const fs = require('fs')
 
 const app = express();
@@ -16,9 +17,34 @@ app.post('/produits', (req, res) => {
     const id = uuid();
     const produit = req.body; 
 
-    fs.writeFileSync(`${directory}/${id}.txt`,JSON.stringify(produit));
+    fs.writeFileSync(path.join(directory,`${id}.txt`) ,JSON.stringify(produit));
     res.sendStatus(201);
 });
+
+app.get('/produits/all', (req, res) => {
+
+    var prds = []; 
+
+    const files = fs.readdirSync(directory);
+
+    files.forEach(file => {
+        const data = fs.readFileSync(path.join(directory, file), 'utf8');
+        prds.push({id:file.split('.')[0], ...JSON.parse(data)});
+    })
+
+    res.json(prds);
+}); 
+
+app.get('/produits/id/:id', (req, res) => {
+
+    const id = req.params.id;
+
+    if(!fs.existsSync(path.join(directory, `${id}.txt`))){
+        return res.sendStatus(404);
+    }
+    const data = fs.readFileSync(path.join(directory, `${id}.txt`), 'utf8'); 
+    res.status(201).json({id:id , ...JSON.parse(data)});
+})
 
 app.listen(port, () => {
     console.log('Serveur lance ....')
